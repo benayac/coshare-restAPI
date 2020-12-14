4ed28cebfefc7ae0ed3be004196df56717a4d62b
Benaya Caesario P <benaya@mamiteam.com>
2020-12-14 13:38:14 +0700

    get courses with filter

--- app/api_router.py
+++ app/api_router.py
@@ -31,11 +31,22 @@ def login(account: LoginAccount, Authorize: AuthJWT = Depends()):
         return request_error(e)
 
 @router.get("/get_courses")
-def get_courses():
+def get_courses(type: int):
     try:
-        courses = db.get_all_courses()
-        response = collection_to_list(courses)
-        return request_success(response)
+        data = []
+        if(type == 0):
+            data = db.get_all_courses()
+        elif(type == 1):
+            data = db.get_courses_from_type("Programming")
+        elif(type == 2):
+            data = db.get_courses_from_type("Data Science")
+        elif(type == 3):
+            data = db.get_courses_from_type("UX Design")
+        elif(type == 4):
+            data = db.get_courses_from_type("Web Development")
+        elif(type == 5):
+            data = db.get_courses_from_type("Marketing")
+        return request_success(serialize_data(data))
     except Exception as e:
         return request_error(e)
 
@@ -60,24 +71,6 @@ def get_course(id: int, Authorize: AuthJWT = Depends()):
     try:
         Authorize.jwt_required()
         data = db.get_course_from_id(id)
-        return request_success(serialize_data(data))
-    except Exception as e:
-        return request_error(e)
-
-@router.get("/filter_course")
-def filter_course(type: int):
-    try:
-        data = []
-        if(type == 0):
-            data = db.get_courses_from_type("Programming")
-        elif(type == 1):
-            data = db.get_courses_from_type("Data Science")
-        elif(type == 2):
-            data = db.get_courses_from_type("UX Design")
-        elif(type == 3):
-            data = db.get_courses_from_type("Web Development")
-        elif(type == 4):
-            data = db.get_courses_from_type("Marketing")
         return request_success(serialize_data(data))
     except Exception as e:
         return request_error(e)
